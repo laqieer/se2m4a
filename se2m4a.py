Fix a bug in SNR calculation: convert signed to unsigned for signal power.

se2m4a.py
@@ -51,7 +51,7 @@ def calculate_SNR(uncompressed_data, decompressed_data) :
     sum_son = np.int64(0)
     sum_mum = np.int64(0)
     for i in range(len(decompressed_data)) :
-        sum_son += int(decompressed_data[i]) * int(decompressed_data[i])
+        sum_son += int(decompressed_data[i] + 128) * int(decompressed_data[i] + 128)
         sub = decompressed_data[i] - uncompressed_data[i]
         sum_mum += sub * sub
     return 10 * math.log10(float(sum_son) / float(sum_mum))