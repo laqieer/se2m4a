Fix calculate_SNR: RuntimeWarning: overflow encountered in byte_scalars.

se2m4a.py
@@ -98,7 +98,7 @@ def calculate_SNR(uncompressed_data, decompressed_data) :
     sum_mum = np.int64(0)
     for i in range(len(decompressed_data)) :
         sum_son += int(decompressed_data[i] + 128) ** 2
-        sub = decompressed_data[i] - uncompressed_data[i]
+        sub = int(decompressed_data[i]) - int(uncompressed_data[i])
         sum_mum += sub ** 2
     return 10 * math.log10(float(sum_son) / float(sum_mum))
 