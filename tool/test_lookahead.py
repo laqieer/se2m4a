6ecbc69640500a87e5af056ce16f21b7cc0007bd
laqieer <laqieer@126.com>
2021-10-13 15:37:01 +0800

    Update lookahead test with PSNR.

--- tool/test_lookahead.py
+++ tool/test_lookahead.py
@@ -6,13 +6,15 @@ sys.path.append("..")
 from se2m4a import *
 import matplotlib.pyplot as plt
 
-lookaheads = range(0, 6)
+lookaheads = range(0, 5)
 SNR_all = []
+PSNR_all = []
 time_all = []
 
 for audio_file in sys.argv[1:]:
     print(audio_file)
     SNR_audio = []
+    PSNR_audio = []
     time_audio = []
     audio_name, audio_ext = os.path.splitext(audio_file)
     audio_freq = audio_name + "-10512" + audio_ext
@@ -36,13 +38,16 @@ for audio_file in sys.argv[1:]:
         end = time.time()
         SNR = calculate_SNR(uncompressed_data, decompressed_data)
         SNR_audio.append(SNR)
+        PSNR = calculate_PSNR(uncompressed_data, decompressed_data)
+        PSNR_audio.append(PSNR)
         t = end - start
         time_audio.append(t)
-        print("lookahead=%d: SNR: %.2fdB Time:%.2fs" % (lookahead, SNR, t))
+        print("lookahead=%d: SNR: %.2fdB PSNR: %.2fdB Time:%.2fs" % (lookahead, SNR, PSNR, t))
     SNR_all.append(SNR_audio)
+    PSNR_all.append(PSNR_audio)
     time_all.append(time_audio)
 
-figure, axis = plt.subplots(1, 2)
+figure, axis = plt.subplots(1, 3)
 
 axis[0].set_title('SNR-Lookahead')
 axis[0].set_xlabel('lookahead')
@@ -51,12 +56,19 @@ axis[0].set_xticks(lookaheads)
 for SNR in SNR_all:
     axis[0].plot(lookaheads, SNR)
 
-axis[1].set_title('Time-Lookahead')
+axis[1].set_title('PSNR-Lookahead')
 axis[1].set_xlabel('lookahead')
-axis[1].set_ylabel('compression time/s')
+axis[1].set_ylabel('PSNR/dB')
 axis[1].set_xticks(lookaheads)
+for PSNR in PSNR_all:
+    axis[1].plot(lookaheads, PSNR)
+
+axis[2].set_title('Time-Lookahead')
+axis[2].set_xlabel('lookahead')
+axis[2].set_ylabel('compression time/s')
+axis[2].set_xticks(lookaheads)
 for time in time_all:
-    axis[1].plot(lookaheads, time)
+    axis[2].plot(lookaheads, time)
 
 plt.tight_layout()
 plt.show()
