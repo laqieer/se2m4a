68aee30ed80812e8c77d013582a4549ed8b00e1e
laqieer <laqieer@126.com>
2021-10-13 15:05:39 +0800

    Update frequency test with PSNR.

--- tool/test_frequency.py
+++ tool/test_frequency.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-import os
 import sys
-import wave
-import aifc
 sys.path.append("..")
 from se2m4a import *
 import matplotlib.pyplot as plt
 
-SNR_all = {}
+SNR_all = []
+PSNR_all = []
 
 for audio_file in sys.argv[1:]:
     SNR_audio = []
+    PSNR_audio = []
     for freq in magic_rates:
         audio_name, audio_ext = os.path.splitext(audio_file)
         audio_freq = audio_name + "-" + str(freq) + audio_ext
@@ -30,15 +29,28 @@ for audio_file in sys.argv[1:]:
         compressed_data, decompressed_data = compress(uncompressed_data)
         SNR = calculate_SNR(uncompressed_data, decompressed_data)
         SNR_audio.append(SNR)
-    SNR_all[os.path.split(audio_file)[-1]] = SNR_audio
+        PSNR = calculate_PSNR(uncompressed_data, decompressed_data)
+        PSNR_audio.append(PSNR)
+    SNR_all.append(SNR_audio)
+    PSNR_all.append(PSNR_audio)
 
-plt.title('SNR-Frequency')
-plt.xlabel('freq/Hz')
-plt.ylabel('SNR/dB')
-plt.xticks(magic_rates)
-plt.xticks(rotation=-30)
-for audio, SNR in SNR_all.items():
-    plt.plot(magic_rates, SNR, label = audio)
-#plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+figure, axis = plt.subplots(1, 2)
+
+axis[0].set_title('SNR-Frequency')
+axis[0].set_ylabel('SNR/dB')
+axis[0].set_xlabel('freq/Hz')
+axis[0].set_xticks(magic_rates)
+axis[0].tick_params('x', labelrotation=-30)
+for SNR in SNR_all:
+    axis[0].plot(magic_rates, SNR)
+
+axis[1].set_title('PSNR-Frequency')
+axis[1].set_ylabel('PSNR/dB')
+axis[1].set_xlabel('freq/Hz')
+axis[1].set_xticks(magic_rates)
+axis[1].tick_params('x', labelrotation=-30)
+for PSNR in PSNR_all:
+    axis[1].plot(magic_rates, PSNR)
+
 plt.tight_layout()
 plt.show()
